e11fb94d49f37e4aaf35944c4e5ecde5a986a276
Matheus Teles <matheus-tdo@outlook.com>
2018-12-20 01:47:55 -0300

    Implement manually port addition

--- src/view/Server.java
+++ src/view/Server.java
@@ -1,16 +1,28 @@
 package view;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
 
 import rmi.Servant;
 
 
 public class Server {
-	public static final int port = 1050;
+	public static int port;
 
 	public static void main(String[] args) throws Exception {
+		if(args.length == 0) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("> Insert port: ");
+			port = Integer.parseInt(scanner.nextLine());
+			scanner.close();
+		} else {
+			port = Integer.parseInt(args[0]);
+		}		
+		
 		Registry registry = LocateRegistry.createRegistry(port);
 		registry.bind("TextEditor", new Servant());
+		
+		
 		
 		System.out.println("> TextEditor service is running at " + port + " port");
 	}
